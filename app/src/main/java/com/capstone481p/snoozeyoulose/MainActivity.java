1a900b1e1e681492b80b92cb41181d434d4245c9
Annika Epperly <epperann@uw.edu>
2023-05-16 09:01:14 -0700

    Basic Authentication working and in app notifications enabled

--- app/src/main/java/com/capstone481p/snoozeyoulose/MainActivity.java
+++ app/src/main/java/com/capstone481p/snoozeyoulose/MainActivity.java
@@ -1,8 +1,11 @@
 package com.capstone481p.snoozeyoulose;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -15,10 +18,12 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.capstone481p.snoozeyoulose.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.installations.FirebaseInstallations;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int SIGN_IN_REQUEST_CODE = 1;
     private ActivityMainBinding binding;
 
 
@@ -27,6 +32,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            // Start sign in/sign up activity
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .build(),
+                    SIGN_IN_REQUEST_CODE
+            );
+        } else {
+            // User is already signed in. Therefore, display
+            // a welcome Toast
+            Toast.makeText(this,
+                            "Welcome " + FirebaseAuth.getInstance()
+                                    .getCurrentUser()
+                                    .getDisplayName(),
+                            Toast.LENGTH_LONG)
+                    .show();
+            // Load chat room contents
+            displayChatMessages();
+        }
+
+
+        
+        FirebaseInstallations.getInstance().getId()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Log.d("Installations", "Installation ID: " + task.getResult());
+                    } else {
+                        Log.e("Installations", "Unable to get Installation ID");
+                    }
+                });
+
+    }
+
+    public void displayChatMessages(){
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -40,8 +82,28 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-        System.out.println(FirebaseInstallations.getInstance().getId());
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SIGN_IN_REQUEST_CODE) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText(this,
+                                "Successfully signed in. Welcome!",
+                                Toast.LENGTH_LONG)
+                        .show();
+                displayChatMessages();
+            } else {
+                Toast.makeText(this,
+                                "We couldn't sign you in. Please try again later.",
+                                Toast.LENGTH_LONG)
+                        .show();
+                // Close the app
+                finish();
+            }
+        }
     }
 
 
